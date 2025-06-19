41c83283323994309270c44d2c3df5397d4af401
chris <chris@dezzeh.com>
2025-06-19 22:37:22 +0100

    Initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Dezzehs Action Figure Things';
+export const SITE_DESCRIPTION = 'Never growing up!';
