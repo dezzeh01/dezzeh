de08147b9a7f282d823db68c6f3ca9f1ff99c853
chris <chris@dezzeh.com>
2025-06-20 08:18:32 +0100

    corrected integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [icon()],
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), icon(),],
 });
