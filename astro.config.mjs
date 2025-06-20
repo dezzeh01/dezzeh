7ff6e5bd64e5278051c44dba195ac7ceca3b5788
chris <chris@dezzeh.com>
2025-06-20 08:15:20 +0100

    added astreo icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+	integrations: [icon()],
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 });
