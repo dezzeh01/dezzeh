6e10b9e7bbc593aeb766a8b22145168515e17020
chris <chris@dezzeh.com>
2025-06-20 10:19:28 +0100

    ameneded consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Dezzehs Action Figure Things';
-export const SITE_DESCRIPTION = 'Never growing up!';
+export const SITE_TITLE = 'Dezzehs Practice Nerc Stuff';
+export const SITE_DESCRIPTION = 'Help, I don't know what I am doing.';
